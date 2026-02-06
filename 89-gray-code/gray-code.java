81e96f320314554b0fe635190690ce652f4afb56
RohitDev <rockee.004@gmail.com>
2026-02-06 22:30:21 +0530

    Time: 4 ms (98.67%) | Memory: 53.7 MB (71.91%) - LeetSync

---
+++ 89-gray-code/gray-code.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        int totalNumbers = 1 << n;
+        for (int i = 0; i < totalNumbers; i++) {
+            result.add(i ^ (i >> 1));
+        }
+        return result;
+    }
+}
